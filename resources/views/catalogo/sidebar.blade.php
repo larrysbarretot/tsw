c1a3241d86e05dc88cf04816c64350c47eae43c8
Larry Steve Barreto Trujillo <larrysbarretot@gmail.com>
2017-06-22 21:30:16 +0000

    avance de correcciones responsive en menu, modal, catalogo y ordenamiento de codigo

---
+++ resources/views/catalogo/sidebar.blade.php
@@ -0,0 +1,49 @@
+<div class="col-md-12">
+  <div id="filtros-container" class="collapse navbar-toggleable-xs">
+    <div class="list-group">
+      <a href="" class="list-group-item list-group-item-action active">
+        <h5 class="list-group-item-heading">Gama</h5>
+      </a>
+      <a href="" class="list-group-item list-group-item-action">
+        Alta <span class="tag tag-pill tag-default pull-xs-right">100</span>
+      </a>
+      <a href="" class="list-group-item list-group-item-action">
+        Media <span class="tag tag-pill tag-default pull-xs-right">50</span>
+      </a>
+      <a href="" class="list-group-item list-group-item-action">
+        Baja <span class="tag tag-pill tag-default pull-xs-right">69</span>
+      </a>
+    </div>
+    <div class="list-group">
+      <a href="" class="list-group-item list-group-item-action active">
+        <h5 class="list-group-item-heading">Conectividad</h5>
+      </a>
+      <a href="" class="list-group-item list-group-item-action">
+        4G <span class="tag tag-pill tag-default pull-xs-right">70</span>
+      </a>
+      <a href="" class="list-group-item list-group-item-action">
+        3G <span class="tag tag-pill tag-default pull-xs-right">50</span>
+      </a>
+    </div>
+    <div class="list-group">
+      <a href="" class="list-group-item list-group-item-action active">
+        <h5 class="list-group-item-heading">Marca</h5>
+      </a>
+      <a href="" class="list-group-item list-group-item-action">
+        Samsung <span class="tag tag-pill tag-default pull-xs-right">70</span>
+      </a>
+      <a href="" class="list-group-item list-group-item-action">
+        iPhone <span class="tag tag-pill tag-default pull-xs-right">50</span>
+      </a>
+      <a href="" class="list-group-item list-group-item-action">
+        HTC <span class="tag tag-pill tag-default pull-xs-right">50</span>
+      </a>
+      <a href="" class="list-group-item list-group-item-action">
+        LG <span class="tag tag-pill tag-default pull-xs-right">50</span>
+      </a>
+      <a href="" class="list-group-item list-group-item-action">
+        Huawei <span class="tag tag-pill tag-default pull-xs-right">50</span>
+      </a>
+    </div>
+  </div>
+</div>
--- resources/views/main/catalogo.blade.php
+++ resources/views/main/catalogo.blade.php
@@ -1,13 +1,13 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
   <head>
-    <!-- Required meta tags always come first -->
+    
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
 
-   <!-- Bootstrap CSS -->
+   
     
     <link rel="stylesheet" type="text/css" href="{{url('CelularesPeru/css/bootstrap-flex.min.css')}}">
     <link rel="stylesheet" type="text/css" href="{{url('/CelularesPeru/css/app.css')}}">
@@ -17,7 +17,7 @@
     <link rel="stylesheet" type="text/css" href="{{url('/CelularesPeru/fontawesome/css/font-awesome.css ')}}">
   </head>
   <body>
-     <!--Header-->
+     
 
 
     <header id="header-container">
@@ -57,7 +57,7 @@
       </div>  
     </header>
 
-    <!--Menu-->
+    
     <div id="menu-container">
       <nav id="navMenu" class="navbar-toggleable-xs navbar navbar-light collapse">
       <div class="container">
@@ -147,9 +147,9 @@
     </div>
 
     
-    <!--/Menu-->
+    
 
-    <!--Celulares-->
+    
         <section id="celulares-container">
       <div class="container">
         <div class="row">
@@ -269,9 +269,9 @@
     </section>
 
 
-    <!--/Celulares-->
+    
 
-     <!--Footer-->
+    
     <footer id="footer-container">
       <div class="contenedor-f">
           <article class="footer-1">
@@ -317,13 +317,13 @@
           </article>
       </div>
     </footer>
-    <!--/Footer-->
+    
 
 
-    <!-- jQuery first, then Tether, then Bootstrap JS. -->
+    
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js" integrity="sha384-THPy051/pYDQGanwU6poAc/hOdQxjnOEXzbT+OuUAFqNqFjL+4IGLBgCJC3ZOShY" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.2.0/js/tether.min.js" integrity="sha384-Plbmg8JY28KFelvJVai01l8WyZzrYWG825m+cZ0eDDS1f7d/js6ikvy1+X+guPIB" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/js/bootstrap.min.js" integrity="VjEeINv9OSwtWFLAtmc4JCtEJXXBub00gtSnszmspDLCtC0I4z4nqz7rEFbIZLLU" crossorigin="anonymous"></script>
     <script src="{{url('/CelularesPeru/js/app.js')}}"></script>
   </body>
-</html>
+</html> -->
